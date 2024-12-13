95e54aec8a563be6921bebbab0c1754374112e99
3mpty9 <cho620@qstruct.ecn.purdue.edu>
2024-12-12 19:33:20 -0500

    mem-cache: Implement adaptive cache
        Added an adaptive cache mechanism to dynamically adjust cache size based
        on workload patterns.
    
    Change-Id: I5410bd627c40a7acdb535bcc077f8edee19e7e1e

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,44 +1,48 @@
-for benchmark in lbm leslie3d #astar milc mcf sjeng namd 
-do 
-    OUTPUT_DIR="results/comp_gcp/$benchmark/"
+for benchmark in lbm leslie3d astar milc #mcf sjeng namd 
+do
+    for l2_size in 256kB 1MB 16MB
 
-    # Check OUTPUT_DIR existence
-    if [[ !(-d "$OUTPUT_DIR") ]]; then
-        mkdir -p $OUTPUT_DIR
-    fi
-    SCRIPT_OUT=$OUTPUT_DIR/runscript_file.log
+    do 
+        OUTPUT_DIR="results/comp_gcp/$l2_size/$benchmark/"
+        C_SIZE = "$l2_size"
 
-    ./build/ECE565-ARM/gem5.opt -d $OUTPUT_DIR configs/spec/spec_se.py \
-    --cpu-type=MinorCPU --l1d_size=64kB --l1i_size=64kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 \
-    --caches --l2cache --l2_size=4MB --mem-size=4GB --maxinsts=10000000 -b $benchmark\
-    --l2_compressor --gcp --gcp_inc 1 --gcp_dec 2 \
-    >> $SCRIPT_OUT
+        # Check OUTPUT_DIR existence
+        if [[ !(-d "$OUTPUT_DIR") ]]; then
+            mkdir -p $OUTPUT_DIR
+        fi
+        SCRIPT_OUT=$OUTPUT_DIR/runscript_file.log
 
-    OUTPUT_DIR="results/comp/$benchmark/"
+        ./build/ECE565-ARM/gem5.opt -d $OUTPUT_DIR --l2_size="$C_SIZE" configs/spec/spec_se.py \
+        --cpu-type=MinorCPU --l1d_size=64kB --l1i_size=64kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 \
+        --caches --l2cache  --mem-size=4GB --maxinsts=10000000 -b $benchmark\
+        --l2_compressor --gcp --gcp_inc 1 --gcp_dec 2 \
+        >> $SCRIPT_OUT
 
-    # Check OUTPUT_DIR existence
-    if [[ !(-d "$OUTPUT_DIR") ]]; then
-        mkdir -p $OUTPUT_DIR
-    fi
-    SCRIPT_OUT=$OUTPUT_DIR/runscript_file.log
+        OUTPUT_DIR="results/comp/$l2_size/$benchmark/"
 
-    ./build/ECE565-ARM/gem5.opt -d $OUTPUT_DIR configs/spec/spec_se.py \
-    --cpu-type=MinorCPU --l1d_size=64kB --l1i_size=64kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 \
-    --caches --l2cache --l2_size=4MB --mem-size=4GB --maxinsts=10000000 -b $benchmark\
-    --l2_compressor \
-    >> $SCRIPT_OUT
+        # Check OUTPUT_DIR existence
+        if [[ !(-d "$OUTPUT_DIR") ]]; then
+            mkdir -p $OUTPUT_DIR
+        fi
+        SCRIPT_OUT=$OUTPUT_DIR/runscript_file.log
 
-    OUTPUT_DIR="results/never/$benchmark/"
+        ./build/ECE565-ARM/gem5.opt -d $OUTPUT_DIR --l2_size="$C_SIZE" configs/spec/spec_se.py \
+        --cpu-type=MinorCPU --l1d_size=64kB --l1i_size=64kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 \
+        --caches --l2cache --mem-size=4GB --maxinsts=10000000 -b $benchmark\
+        --l2_compressor \
+        >> $SCRIPT_OUT
 
-    # Check OUTPUT_DIR existence
-    if [[ !(-d "$OUTPUT_DIR") ]]; then
-        mkdir -p $OUTPUT_DIR
-    fi
-    SCRIPT_OUT=$OUTPUT_DIR/runscript_file.log
+        OUTPUT_DIR="results/never/$l2_size/$benchmark/"
 
-    ./build/ECE565-ARM/gem5.opt -d $OUTPUT_DIR configs/spec/spec_se.py \
-    --cpu-type=MinorCPU --l1d_size=64kB --l1i_size=64kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 \
-    --caches --l2cache --l2_size=4MB --mem-size=4GB --maxinsts=10000000 -b $benchmark\
-    >> $SCRIPT_OUT
+        # Check OUTPUT_DIR existence
+        if [[ !(-d "$OUTPUT_DIR") ]]; then
+            mkdir -p $OUTPUT_DIR
+        fi
+        SCRIPT_OUT=$OUTPUT_DIR/runscript_file.log
+
+        ./build/ECE565-ARM/gem5.opt -d $OUTPUT_DIR --l2_size="$C_SIZE" configs/spec/spec_se.py \
+        --cpu-type=MinorCPU --l1d_size=64kB --l1i_size=64kB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 \
+        --caches --l2cache --mem-size=4GB --maxinsts=10000000 -b $benchmark\
+        >> $SCRIPT_OUT
+    done
 done
-
